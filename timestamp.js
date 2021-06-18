8b04f392ce042477452baac67756241ef5efc40f
Derek Jamison <codeallnight@outlook.com>
2021-06-17 19:27:06 -0500

    Fix timestamp to be offset by localtime instead of UTC.

--- timestamp.js
+++ timestamp.js
@@ -1,7 +1,9 @@
 /* eslint-disable linebreak-style */
 
 function timestampNow() {
-  const d = new Date().toISOString();
+  let n = new Date();
+  n = new Date(Date.now() - (n.getTimezoneOffset() * 60000));
+  const d = n.toISOString();
   // Format is MM-DD-YYYY HH:MM:SS
   return `${d.substring(5, 10) + d.substring(4, 5) + d.substring(0, 4)} ${d.substring(11, 19)}`;
 }
