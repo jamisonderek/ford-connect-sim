52a3c3803e9c09ea299ae0dcb3ffceda361f407a
Derek Jamison <codeallnight@outlook.com>
2021-06-24 00:22:55 -0500

    invalid vehicleId, wrong length id, bad auth token.

--- test/unlock.js
+++ test/unlock.js
@@ -78,5 +78,118 @@ describe('Unlock tests', () => {
           });
       });
     });
+    describe('with invalid vehicleId', () => {
+      const badVehicleId = 'BADBADBADBADBADBADBADBADBADBADBA';
+      const url = `/api/fordconnect/vehicles/v1/${badVehicleId}/unlock`;
+      it('it should return HTTP 404 status code', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            res.should.have.status(404);
+            done();
+          });
+      });
+      it('it should return body with error code 4002', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.error.code).to.equal(4002);
+            done();
+          });
+      });
+      it('it should return body with error statusCode NOT_FOUND', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.error.statusCode).to.equal('NOT_FOUND');
+            done();
+          });
+      });
+      it('it should return body with status of FAILED', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.status).to.equal('FAILED');
+            done();
+          });
+      });
+      it('it should return body with commandStatus of EMPTY', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.commandStatus).to.equal('EMPTY');
+            done();
+          });
+      });
+    });
+    describe('with wrong length vehicleId', () => {
+      const shortVehicleId = 'TOOSHORTID';
+      const url = `/api/fordconnect/vehicles/v1/${shortVehicleId}/unlock`;
+      it('it should return HTTP 400 status code', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            res.should.have.status(400);
+            done();
+          });
+      });
+      it('it should return errorCode 400', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.errorCode).to.equal('400');
+            done();
+          });
+      });
+      it('it should return errorMessage containing "Invalid vehicleId"', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.errorMessage).to.contain('Invalid vehicleId');
+            done();
+          });
+      });
+      it('it should return errorMessage containing "size must be between 32 and 32"', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(authToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            expect(res.body.errorMessage).to.contain('size must be between 32 and 32');
+            done();
+          });
+      });
+    });
+    describe('with invalid auth token', () => {
+      const anyVehicleId = mockVehicles.ev1.vehicleId;
+      const url = `/api/fordconnect/vehicles/v1/${anyVehicleId}/unlock`;
+      const invalidAuthToken = 'INVALID TOKEN';
+      it('it should return HTTP 401 status code', (done) => {
+        chai.request(server)
+          .post(url)
+          .auth(invalidAuthToken, { type: 'bearer' })
+          .send()
+          .end((err, res) => {
+            res.should.have.status(401);
+            done();
+          });
+      });
+    });
   });
 });
